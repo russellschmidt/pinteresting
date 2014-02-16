19737a9135b4ec43d90b2851a719b524624450cf
russellschmidt <russell.schmidt@me.com>
2014-02-15 22:23:38 -0800

    added line to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,8 @@ Pinteresting::Application.configure do
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+      :s3_endpoint => 's3-us-west-1.amazonaws.com'
     }
   }
 end
